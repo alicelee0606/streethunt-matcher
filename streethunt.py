c63ebf598857ded75580662a2394c2367ed299ec
Acsa Lu <acsa0210@gmail.com>
2014-01-03 20:16:32 +0800

    Now can query nearby points in the database.

--- streethunt.py
+++ streethunt.py
@@ -6,29 +6,29 @@ import cv2
 import sys, os
 import urllib
 import csv
+import geopy
+from geopy import distance
 
-db = None
+data_points = []
 dbname = "database"
 datafilename = "data.csv"
+NEAR_DISTANCE_THRESHOLD = 30
 API_KEY = "AIzaSyBS-HaMAHhazScAOwdTOaclJEGBNptWFss"
 
-def fetch_data(street_name=u"信義路", direction=u"東"):
+def fetch_data(nodes):
     print "Fetching data for " + street_name
-    query = ParsePy.ParseQuery("Node")
-    query = query.limit(10000).eq("streetName", street_name).eq("direction", direction) 
-    nodes = query.fetch()
-    db = []
+    data = []
     dirname = "%s_%s" % (street_name, direction)
     for n in nodes:
         print str(nodes.index(n) + 1) + "/" + str(len(nodes)) + "(" + str(n.lat) + ", " + str(n.lng) + ")"
-        db.append([n.lat, n.lng])
+        data.append([n.lat, n.lng])
         for i in range(8):
             url = imageurl(n.lat, n.lng, i * 45)
             save_image(url, "sv_%f_%f_%d.jpg" % (n.lat, n.lng, i), dirname)
     
     f = open(os.path.join(dbname, dirname, datafilename), 'w')
     w = csv.writer(f)
-    w.writerows(db)
+    w.writerows(data)
     f.close()
 
 def imageurl(lat, lng, heading=0, pitch=0):
@@ -58,18 +58,27 @@ def load_db(street_name=u"信義路", direction=u"東"):
     print "There're %d nodes on the server." % (len(nodes))
 
     if not os.path.exists(dirpath):
-        fetch_data(street_name, direction)
+        fetch_data(nodes)
     else:
         files = [f for f in os.listdir(dirpath) if f[-3:] == "jpg"]
         f = open(os.path.join(dbname, dirname, datafilename), 'r')
         if not ((len(files) / 8 == len(nodes)) and (len(nodes) == sum(1 for row in csv.DictReader(f)) + 1)):
-            fetch_data(street_name, direction)
+            fetch_data(nodes)
         else:
             print "Your database is up-to-date."
+    
+    global data_points
+    data_points = [geopy.Point(n.lat, n.lng) for n in nodes]
 
+def get_near_img(lat, lnt, threshold=NEAR_DISTANCE_THRESHOLD):
+    p = geopy.Point(lat, lnt)
+    near_points = [dp for dp in data_points if distance.distance(p, dp).m < threshold]
+    
+    #for np in near_points:
+    #    for i in range(8):
+    #        print "sv_%f_%f_%d.jpg" % (np.latitude, np.longitude, i)
 
-def get_nearest_pictures(lat, lnt):
-    print "Nothing here yet ha"
+    return near_points
 
 def camera_capture():
     cap = cv2.VideoCapture(0)
@@ -86,3 +95,5 @@ def camera_capture():
 if __name__ == '__main__':
     init_parse()
     load_db()
+    get_near_img(25.033245507599506, 121.54339224100113)
+
