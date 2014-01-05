c896f25249edb029ecb28e7e702f30abf28d2a25
Acsa Lu <acsa0210@gmail.com>
2014-01-05 17:08:36 +0800

    Add util.py

--- util.py
+++ util.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.cluster.vq import kmeans,vq
 from numpy import *
+import os
 
 def init_parse():
     ParsePy.APPLICATION_ID = "0jrcxKZWUvcY7BXVmCj2hWmSYS2fDcJfWPbGW9p9"
@@ -20,7 +21,12 @@ def get_specific_frame(v, frame):
     f =  v.read()[1]
     return f    
 
-def get_descriptor(img, n_slice=1):
+def get_descriptor(f_path=None, img=None, n_slice=1):
+    if f_path is None and img is None:
+        print "ERROR: No input specified."
+        return
+    if img is None:
+        img = cv2.imread(f_path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     sift = cv2.SIFT(0, 3, 0.04, 10, 1.6)
@@ -30,13 +36,19 @@ def get_descriptor(img, n_slice=1):
     des = []
 
     for i in range(0, n_slice):
-        mask[i, 0:(h / 2), i * w / n_slice:(i + 1) * w / n_slice] = 255
+        mask[i, 0:h/2, i * w / n_slice:(i + 1) * w / n_slice] = 255
         kp.append(sift.detect(img, mask[i]))
-        kp[i], d = sift.compute(img, kp[i])
+        d = np.array([])
+        #print '[',i,'] ', len(kp[i])
+        if len(kp[i]) > 0:
+            #print "compute", i
+            kp[i], d = sift.compute(img, kp[i])
+        
         des.append(d)
+        #print des[i]
     
     for i in range(0, n_slice):
-        print '    #des:',len(des[i])
+        print '     des#',i,':',len(des[i])
     return kp, des
 
 def get_min_dist(ds1, ds2):
@@ -49,3 +61,19 @@ def get_min_dist(ds1, ds2):
             if min_dist < 0 or d < min_dist:
                 min_dist = d
     return min_dist
+
+def get_img_file_name(lat, lnt, direction):
+    return  "sv_%f_%f_%d.jpg" % (lat, lnt, direction)
+
+def save_image(url, dbname, dirname, filename):
+    if not os.path.exists(dbname):
+        os.mkdir(dbname)
+    dirpath = os.path.join(dbname, dirname)
+    if not os.path.exists(dirpath):
+        os.mkdir(dirpath)
+    filepath = os.path.join(dbname, dirname, filename)
+    if not os.path.isfile(filepath):
+        urllib.urlretrieve(url, filepath)
+
+if __name__ == '__main__':
+    get_descriptor(f_path="database/XinYiRd-East/sv_25.033365_121.537655_3.jpg", n_slice=4)
